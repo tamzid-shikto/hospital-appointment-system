4c5fa5d9c6298a07d26ca8e22b09ec67ebd31192
tamzid-shikto <shikto.49.cr@gmail.com>
2026-08-13 22:48:27 +0600

    again fixed static route

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "../public")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
